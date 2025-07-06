7460f46fae072c9677de7aaebf8741f2ae08d3b5
Beshawy <elbeshwaya006@gmail.com>
2025-07-06 06:19:22 +0300

    new

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  swcMinify: true,
-  // experimental: {
-  //   // Required: for next 13
-  //   appDir: true
-  // }
   i18n: {
     locales: ["ar", "en"],
     defaultLocale: "ar",
-    localeDetection: true
+    localeDetection: false
   },
   images: {
     domains: ["pasteboard.co","images.unsplash.com", "s3-alpha-sig.figma.com", "res.cloudinary.com"]
